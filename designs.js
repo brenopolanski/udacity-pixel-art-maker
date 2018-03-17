48a0539836a218c817ba3fc3edd8ae33d2837817
brenopolanski <breno.polanski@gmail.com>
2018-03-17 20:33:13 -0300

    Add code

--- designs.js
+++ designs.js
@@ -1,10 +1,38 @@
-// Select color input
-// Select size input
+(() => {
+  // set global variables
+  const $sizePicker  = document.querySelector('#sizePicker');
+  const $colorPicker = document.querySelector('#colorPicker');
+  const $table = document.querySelector('#pixelCanvas');
 
-// When size is submitted by the user, call makeGrid()
+  // add listener to select grid size
+  $sizePicker.addEventListener('submit', (event) => {
+    // prevent page refresh on submit
+    event.preventDefault();
 
-function makeGrid() {
+    // get input data and draw grid
+    const inputWidth = document.querySelector('#inputWidth').value;
+    const inputHeight = document.querySelector('#inputHeight').value;
+    makeGrid(inputWidth, inputHeight);
+  });
 
-// Your code goes here!
+  // Draw grid
+  function makeGrid(width, height) {
+    $table.innerHTML = '';
 
-}
+    for (let row = 0; row < width; row++) {
+      let newRow = $table.insertRow();
+
+      for (let cell = 0; cell < height; cell++) {
+        // add new cell with listener to change color
+        let newCell = newRow.insertCell();
+
+        newCell.onclick = changeColor;
+      }
+    }
+  }
+
+  // change the color of the clicked cell to current color
+  function changeColor() {
+    this.style.background = $colorPicker.value;
+  }
+})();
